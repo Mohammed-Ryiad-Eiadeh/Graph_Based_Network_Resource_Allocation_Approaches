b0673b9971c1ca30fb83cf41f0c796cf84b7c1e6
Mohammed Al-Eiadeh <93108547+Mohammed-Ryiad-Eiadeh@users.noreply.github.com>
2024-03-11 13:32:07 -0400

    Add files via upload

---
+++ Resource-Allocation-PageRank-And-Graph-Methods/src/main/java/MainPackage/org/BehavioralDefenderMain.java
@@ -0,0 +1,90 @@
+package MainPackage.org;
+
+import Attack_Defence_Graph.org.Graph;
+import Attack_Defence_Graph.org.GraphData;
+import ConcurrentAttacks.org.ConcurrentAttack;
+import CostFunction.org.CostFunction;
+import Defender.org.Defenders;
+import GraphAnalysisAndDefense.org.BehavioralDefender;
+
+/**
+ * This class is used as the main class for the first scenario according to bahavioral defender
+ */
+public class BehavioralDefenderMain {
+    public static void main(String[] args) {
+        System.out.println("The performance of Behavioral Defender ----------");
+        // Select the test case or the graph; construct the defenders; construct the adjacensy matrix; display the graph.
+        var task = new GraphData(Graph.Figure3);
+        var attackDefenceGraph = task.getAttackDefenceGraph();
+        var AdjMat = task.getAdjacencyMatrix(attackDefenceGraph);
+        var assetLossVec = task.getNodeAssetsLossValues();
+        task.Display(AdjMat);
+
+        // Generate paths by genetic algorithm from each entry node to each asset
+        var concurrentAttackers = new ConcurrentAttack(AdjMat,
+                assetLossVec,
+                500,
+                0.2,
+                0.4,
+                0.6,
+                1000);
+        var cocurrentAttacks = concurrentAttackers.getPaths();
+
+        // Calculate the cost for the generated paths before allocating the resources
+        var assets = cocurrentAttacks.keySet();
+        var costFunctionBeforeAllocation = new CostFunction(AdjMat, assetLossVec);
+        var CostsBeforeAllocation = 0.0d;
+        for (var asset : assets) {
+            var pathsToThisAsset = cocurrentAttacks.get(asset);
+            for (var path : pathsToThisAsset) {
+                CostsBeforeAllocation += costFunctionBeforeAllocation.computeCost(path);
+            }
+            CostsBeforeAllocation += CostsBeforeAllocation / pathsToThisAsset.size();
+        }
+        System.out.println("Before the allocation, the cost of these attacks approaching the assets successfully is : "
+                + CostsBeforeAllocation);
+
+        // Set the spare budget of security resourcess for each defender
+        Defenders.spareBudget_D = 5d;
+
+        // This code segment is referred to allocate spare defenders investments on the most potential path (top-1) according to behavioral defender
+        // Set the spare budget of security resourcess for each defender
+        var budget = Defenders.spareBudget_D;
+
+        // Apply the behavioral defender
+        var startTime = System.currentTimeMillis();
+        var behavioralDefender = new BehavioralDefender(AdjMat, 0.5f);
+        var newWeights = behavioralDefender.applyBehavioralDefendingForResourceAllocation();
+        var sumOfProbWeighting = 0.0d;
+        for (double[] newWeight : newWeights) {
+            for (var j = 0; j < newWeights[0].length; j++) {
+                sumOfProbWeighting += newWeight[j];
+            }
+        }
+        for (var i = 0; i < AdjMat.length; i++) {
+            for (var j = 0; j < AdjMat[0].length; j++) {
+                if (AdjMat[i][j] > 0) {
+                    var edge = attackDefenceGraph[i][j];
+                    edge.setInvest_D(edge.addSpareInvestFor_D(newWeights[i][j] / sumOfProbWeighting * budget));
+                }
+            }
+        }
+        var endTime = System.currentTimeMillis();
+        // Update the adjacensy matrix
+        AdjMat = task.getAdjacencyMatrix(attackDefenceGraph);
+        var costFunctionAfterAllocation = new CostFunction(AdjMat, assetLossVec);
+        var CostsAfterAllocation = 0.0d;
+        for (var asset : assets) {
+            var pathsToThisAsset = cocurrentAttacks.get(asset);
+            for (var path : pathsToThisAsset) {
+                CostsAfterAllocation += costFunctionAfterAllocation.computeCost(path);
+            }
+            CostsAfterAllocation += CostsAfterAllocation / pathsToThisAsset.size();
+        }
+        System.out.println("After the allocation, the cost of these attacks approaching the assets successfully is : "
+                + CostsAfterAllocation);
+        System.out.println("The relative reduction of the cost of these attacks approaching the assets successfully is : "
+                + Math.abs(CostsBeforeAllocation - CostsAfterAllocation) / CostsBeforeAllocation * 100 + "\t%");
+        System.out.println("The duration time of the allocation process took : " + (endTime - startTime) + " ms");
+    }
+}
